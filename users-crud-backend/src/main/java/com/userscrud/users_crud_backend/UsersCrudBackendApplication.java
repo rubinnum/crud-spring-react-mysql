f6dd76c9e7402f6fd1afbe76ca25b6122e4dae11
rubinnum <romarypiak@gmail.com>
2024-09-03 16:39:23 +0200

    init commit

---
+++ users-crud-backend/src/main/java/com/userscrud/users_crud_backend/UsersCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.userscrud.users_crud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersCrudBackendApplication.class, args);
+	}
+
+}
---
+++ users-crud-backend/src/test/java/com/userscrud/users_crud_backend/UsersCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userscrud.users_crud_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
